36a27648fd7d9f1cf15f95d56656d63be0ef0fee
MYpotato <mypotato732@gmail.com>
2026-03-17 03:19:27 +0700

    hot fix 4

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from './generated/prisma/client';
-import * as Enums from './generated/prisma/enums';
+import { PrismaClient } from './generated/prisma';
+import * as Enums from './generated/prisma';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
